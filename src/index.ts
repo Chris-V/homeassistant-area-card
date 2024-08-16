84dfb1bfb5b42801a0e073e083fdcc183510f2e2
Christopher Viel <viel.christopher@gmail.com>
2024-08-15 21:35:57 -0400

    Project structure

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { AreaCard } from "./card";
+
+declare global {
+  interface Window {
+    customCards: Array<Object>;
+  }
+}
+
+customElements.define("area-card", AreaCard);
+
+window.customCards = window.customCards || [];
+window.customCards.push({
+  type: "area-card",
+  name: "Turn an entity on and off",
+  description: "A pretty card with everything related to an area.",
+});
+
