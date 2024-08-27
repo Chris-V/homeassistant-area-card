1032b13b237e10ab418a5b5addd0718d0deb574b
Christopher Viel <viel.christopher@gmail.com>
2024-08-26 22:59:56 -0400

    wip:

--- src/terrrium-card/terrarium-card.styles.ts
+++ src/terrrium-card/terrarium-card.styles.ts
@@ -12,16 +12,15 @@ export default css`
   gap: 4px;
 }
 
-.controls > div:first-child {
-  grid-column: 3 / span 4;
+.controls > div {
+  grid-column-end: span 4;
 }
 
-.controls > div:nth-child(2) {
-  grid-column: 7 / span 4;
+.controls > div:first-child {
+  grid-column-start: 3;
 }
 
 .controls > div:nth-child(n + 3) {
   grid-row-start: 2;
-  grid-column: span 4;
 }
 `;
