f950a9067525e275f61d715e9925856e4768d450
Christopher Viel <viel.christopher@gmail.com>
2024-08-28 20:41:29 -0400

    wip:

--- src/terrrium-card/terrarium-card.ts
+++ src/terrrium-card/terrarium-card.ts
@@ -24,11 +24,15 @@ export interface TerrariumCardConfig extends LovelaceCardConfig {
 
 @customElement('terrarium-card')
 export class TerrariumCard extends LitElement implements LovelaceCard<TerrariumCardConfig> {
-  @property({ attribute: false }) hass!: HomeAssistant
+  @state() private config?: TerrariumCardConfig;
 
-  @state() private config!: TerrariumCardConfig;
+  #hass?: HomeAssistant
 
   static styles = styles;
+
+  set hass(hass: HomeAssistant) {
+    this.#hass = hass;
+  }
 
   setConfig(config: TerrariumCardConfig): void {
     if (!config.area) {
@@ -43,19 +47,19 @@ export class TerrariumCard extends LitElement implements LovelaceCard<TerrariumC
   }
 
   protected render() {
-    if (!this.config || !this.hass) {
+    if (!this.config || !this.#hass) {
       return nothing;
     }
 
     const controls = this.config.controls || [];
     const footer = controls.filter((control) => control.footer);
-    const problemsEntity = this.config.problems ? this.hass.states[this.config.problems] : undefined;
-    const hasProblems = problemsEntity?.state == 'on';
+    const problemsEntity = this.config.problems ? this.#hass.states[this.config.problems] : undefined;
+    const hasProblems = problemsEntity?.state == 'off';
 
     return html`
       <area-card-layout
         class=${classMap({ 'has-problems': hasProblems })}
-        .hass=${this.hass}
+        .hass=${this.#hass}
         .areaId=${this.config.area}
         .color=${this.config.color}
         .header=${false}
@@ -63,7 +67,7 @@ export class TerrariumCard extends LitElement implements LovelaceCard<TerrariumC
         ${footer.map((control) => html`
           <entity-state-icon
             slot="controls"
-            .hass=${this.hass}
+            .hass=${this.#hass}
             .entity=${control.entity}
             .icon=${control.icon}
             .tag=${control.tag}
@@ -79,8 +83,8 @@ export class TerrariumCard extends LitElement implements LovelaceCard<TerrariumC
               slot="controls"
               icon="mdi:alert-circle"
               name="Problems"
-              .hass=${this.hass}
-              .entity=${this.config.problems}
+              .hass=${this.#hass}
+              .entity=${this.config?.problems}
             ></entity-state-icon>
           `)}
         </div>
@@ -89,24 +93,26 @@ export class TerrariumCard extends LitElement implements LovelaceCard<TerrariumC
           ${controls.map((control) => this.createControlTemplate(control))}
         </div>
 
-        <div class="settings-panel">
-          ${this.config.settings?.map((setting) => this.createSettingRowTemplate({ ...setting }))}
-        </div>
+        ${when(this.config.settings?.length, () => html`
+          <div class="settings-panel">
+            ${this.config?.settings?.map((setting) => this.createSettingRowTemplate({ ...setting }))}
+          </div>
+        `)}
       </area-card-layout>
     `;
   }
 
   private createControlTemplate(control: TerrariumControlConfig): TemplateResult {
-    const powerState = control.power_entity && this.hass?.states[control.power_entity];
-    const energyState = control.energy_entity && this.hass?.states[control.energy_entity];
+    const powerState = control.power_entity && this.#hass?.states[control.power_entity];
+    const energyState = control.energy_entity && this.#hass?.states[control.energy_entity];
     const stateLabel = [
-      powerState && this.hass?.formatEntityState(powerState),
-      energyState && this.hass?.formatEntityState(energyState),
+      powerState && this.#hass?.formatEntityState(powerState),
+      energyState && this.#hass?.formatEntityState(energyState),
     ].filter(x => !!x).join(' ⁓ ');
 
     return html`
       <entity-state-icon
-        .hass=${this.hass}
+        .hass=${this.#hass}
         .entity=${control.entity}
         .icon=${control.icon}
         .tag=${control.tag}
@@ -123,7 +129,7 @@ export class TerrariumCard extends LitElement implements LovelaceCard<TerrariumC
   private createSettingRowTemplate(options: LovelaceCardOptions) {
     const rowPromise = window.loadCardHelpers().then(({ createRowElement }) => {
       const row = createRowElement(options);
-      row.hass = this.hass;
+      row.hass = this.#hass;
       return html`<div class="setting-row">${row}</div>`;
     });
     return until(rowPromise, nothing);
